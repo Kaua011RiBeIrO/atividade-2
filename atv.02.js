febdac25b76c98139a666c6dce584f41c264f30d
Kaua011RiBeIrO <102737682+Kaua011RiBeIrO@users.noreply.github.com>
2022-05-05 10:50:59 -0300

    Add files via upload

---
+++ atv.02.js
@@ -0,0 +1,34 @@
+function meunome (a){
+    console.log(a);
+    return
+}
+
+meunome('Kauã');
+meunome('Miguelito');
+meunome('bluno');
+
+//function : cria uma função;
+//toda função tem um nome;
+//parametros;
+
+function soma(a,b){
+    console.log(a+b);
+    return
+}
+soma(7,3);
+
+//atv=c
+
+function mult(c,d){
+    console.log(c*d);
+    return
+}
+mult(7,3);
+
+ 
+function medía (a,b,c,d){
+    let res = (a+b+c+d)/4
+    console.log(res)
+    return
+}
+medía(3,7,4,6)
